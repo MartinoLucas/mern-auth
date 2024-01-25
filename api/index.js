b2cbb539976af381ba25c443cad32994794a9ea5
Lucas Martino <lucasmartino241@gmail.com>
2024-01-25 20:43:17 -0300

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(()=>{console.log("Database connected correctly")})
+    .catch((e) => {console.log("An error has ocurred: ",e)});
 const app = express();
 
 app.listen(3000, () => {
